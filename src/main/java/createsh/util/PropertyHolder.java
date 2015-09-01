6a929338be0fd893f84208d67b9e3fbf3b4dfe9c
darlingtld <darlingtld@qq.com>
2015-09-01 21:26:00 +0800

    saturday morning

--- src/main/java/createsh/util/PropertyHolder.java
+++ src/main/java/createsh/util/PropertyHolder.java
@@ -25,12 +25,21 @@ public class PropertyHolder {
     public static final String APPSECRET = prop.getProperty("wechat.app_secret");
     public static final String SERVER = prop.getProperty("wechat.server");
 
+    public static final String MENU_SHOPPING_MALL="购物专区";
     public static final String MENU_GO_ORDER = "我要下单";
     public static final String MENU_MY_ORDER = "我的订单";
-    public static final String MENU_MY_LIANGYUAN = "我的优品";
-    public static final String MENU_MY_COUPON = "我的卡券";
-    public static final String MENU_NEW_PRODUCT = "新鲜资讯";
-    public static final String MENU_ABOUT_US = "关于我们";
-    public static final String MENU_POST_SALE_SERVICE = "售后服务";
+    public static final String MENU_MY_ACCOUNT = "我的账户";
+
+    public static final String MENU_LIANGYUAN_ENCYCLO = "梁源百科";
+    public static final String MENU_ABOUT_LIANGYUAN = "关于梁源";
+    public static final String MENU_RICE_KNWOLEDGE = "稻谷知识";
+    public static final String MENU_COOK_TRICKS = "煮饭窍门";
+
+    public static final String MENU_POST_SALE_SERVICE = "售后信息";
+    public static final String MENU_FEEDBACK = "问题反馈";
+    public static final String MENU_CONSIGNEE_INFO = "收货信息";
+    public static final String MENU_POST_SALE_RULE = "售后规则";
+    public static final String MENU_SHOP_INFO = "商户信息";
+    public static final String MENU_ONLINE_CS = "在线客服";
 
 }
