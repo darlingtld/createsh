6dfc99435bf5925cf1ea04c3efc9ec426a01f772
darlingtld <darlingtld@qq.com>
2015-09-29 10:22:27 +0800

    dispatch

--- src/main/java/createsh/dao/SupportDao.java
+++ src/main/java/createsh/dao/SupportDao.java
@@ -2,10 +2,12 @@ package createsh.dao;
 
 import createsh.pojo.CSMessage;
 import createsh.pojo.Feedback;
+import org.hibernate.Query;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import java.util.Calendar;
 import java.util.List;
 
 /**
@@ -17,12 +19,16 @@ public class SupportDao {
     @Autowired
     private SessionFactory sessionFactory;
 
-    public void saveFeedback(Feedback feedback){
+    public void saveFeedback(Feedback feedback) {
         sessionFactory.getCurrentSession().save(feedback);
     }
 
     public List<CSMessage> getCSMessageList() {
-        return sessionFactory.getCurrentSession().createQuery(String.format("from CSMessage order by timestamp desc")).list();
+        Query query = sessionFactory.getCurrentSession().createQuery(String.format("from CSMessage where timestamp>:ts order by timestamp desc"));
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.HOUR, -48);
+        query.setTimestamp("ts", calendar.getTime());
+        return query.list();
     }
 
     public void saveCSMessage(CSMessage csMessage) {
